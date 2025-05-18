560aee2b19efe33c554068884f09dd79d5e07539
Encryption-c08 <hawaiianmakana08+1@gmail.com>
2025-05-18 03:43:54 -0700

    Changed mod icon Changed mod name for easy searching Added error handling & fixed the issue where webm files would break the addon

--- src/main/java/anticope/esixtwoone/sources/ESixTwoOne.java
+++ src/main/java/anticope/esixtwoone/sources/ESixTwoOne.java
@@ -21,16 +21,31 @@ public class ESixTwoOne extends Source {
 
     @Override
     public String randomImage(String filter, Size size) {
-        int pageNum = random.nextInt(1, maxPage);
+        if (maxPage < 1) maxPage = 1;
+        int pageNum = random.nextInt(1, maxPage + 1);
         JsonObject result = Http.get(domain + "/posts.json?limit=320&tags="+filter+"&page="+ pageNum).sendJson(JsonObject.class);
         if (result.get("posts") instanceof JsonArray array) {
             if(array.size() <= 0) {
                 maxPage = pageNum - 1;
                 return null;
             }
-            if (array.get(random.nextInt(array.size())) instanceof JsonObject post) {
-                return post.get(size.toString()).getAsJsonObject().get("url").getAsString();
+            for (int tries = 0; tries < array.size(); tries++) {
+                JsonObject post = array.get(random.nextInt(array.size())).getAsJsonObject();
+            
+                if (!post.has(size.toString())) continue;
+            
+                JsonObject sizeObj = post.get(size.toString()).getAsJsonObject();
+            
+                if (!sizeObj.has("url")) continue;
+            
+                String url = sizeObj.get("url").getAsString();
+            
+                if (url.endsWith(".webm")) continue;
+            
+                return url;
             }
+            return null; // no valid image found
+            
         }
         return null;
     }
